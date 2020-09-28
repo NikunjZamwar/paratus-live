37a5fb8c6c46cc48d4f6748c53ca9ec0105ce182
NikunjZamwar <43301538+NikunjZamwar@users.noreply.github.com>
2020-09-28 01:29:36 -0500

    Add media root

--- paratus/paratus/settings.py
+++ paratus/paratus/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'static'),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
